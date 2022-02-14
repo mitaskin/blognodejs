3f81e2b17c1f4b99e74bd3ebc580e79f47aaa036
mitaskin <mi.taskin027@gmail.com>
2022-02-14 10:46:08 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const http = require('http')
+const fs = require('fs') //Filesystem
+ 
+const hostname = '127.0.0.1'
+const port = 3000
+
+const indexPage = fs.readFileSync('index.html')
+const aboutPage = fs.readFileSync('about.html')
+const contactPage = fs.readFileSync('contact.html')
+
+const server = http.createServer((req, res) => {
+
+    if (req.url === '/') {
+        return res.end(indexPage)
+    } else if (req.url === '/about') {
+        return res.end(aboutPage)
+    } else if (req.url === '/contact') {
+        return res.end(contactPage)
+    } else {
+        res.statusCode = 404
+        res.end('Sayfa Bulunamadı')
+    }
+
+})
+
+server.listen(port, hostname, () => {
+    console.log(`Server Çalışıyor, http://${hostname}:${port}/ `)
+})
